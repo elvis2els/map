a9c1f1ae15162b21983957746c83627883cc94f3
elvis2els <yulong_elvis@foxmail.com>
2016-05-03 20:13:50 +0800

    analize Time interval by python3

--- analize/analzeTime.py
+++ analize/analzeTime.py
@@ -22,7 +22,7 @@ def countTime(lines, count):
         posttime = time.mktime(time.strptime(
             lines[i][0:19], '%Y-%m-%d %H:%M:%S'))
         chtime = posttime - pretime
-        if not count.has_key(chtime):
+        if chtime not in count:
             count[chtime] = 1
         else:
             count[posttime - pretime] += 1
@@ -32,29 +32,31 @@ def countTime(lines, count):
 def writeLines(day):
     dirnames = os.listdir(path)
     count = {}
-    begin=time.time()
+    begin = time.time()
+    i = 1
     for cabid in dirnames:
         try:
             ifs = open(path + cabid + '/' + day + '.txt', 'r')
         except Exception as e:
-            print("Can't open file: {0} , error: {1}".format(
-                cabid + '/' + day, e))
             continue
         lines = ifs.readlines()
         ifs.close()
         if len(lines) < 10:
             efs = open(outputPath + 'error.log', 'a')
-            efs.write('cabid: {0}, day: {1}'.format(cabid, day))
+            efs.write('cabid: {0}, day: {1}\n'.format(cabid, day))
             efs.close()
             continue
         countTime(lines, count)
+        if i % 100 == 0:
+            print('day: {1}, finish {0}'.format(i, day))
+        i += 1
     ofs = open(outputPath + day + '.txt', 'w')
     outlines = []
-    for chtime, times in count:
-        outlines.append('{0},{1}'.format(chtime, times))
+    for chtime, times in count.items():
+        outlines.append('{0},{1}\n'.format(chtime, times))
     ofs.writelines(outlines)
     ofs.close()
-    print('finish: {0}, using time: {1}'.format(day, time.time()-begin))
+    print('finish: {0}, using time: {1}'.format(day, time.time() - begin))
 
 
 def main():

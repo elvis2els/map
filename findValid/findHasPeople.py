3908179a800be7fb5a07ad3aaf9eceaeda86a2f7
elvis2els <yulong_elvis@foxmail.com>
2016-06-04 10:13:05 +0800

    find all path that has people

--- findValid/findHasPeople.py
+++ findValid/findHasPeople.py
@@ -2,17 +2,13 @@ import time
 import argparse
 import os
 
-def main():
-    id = '2'
-    day = '20151030'
-    inputpath = '/dataPool/map/2015cabGPS_splitpath/' + id + '/' + day + '.txt'
-    outpath = '/dataPool/map/hasPeople/' + id
-    if not os.path.exists(outpath):
-        os.mkdir(outpath)
-    ifs = open(inputpath, 'r')
+
+def findHasLine(filepath, outpath):
+    ifs = open(filepath, 'r')
     lines = ifs.readlines()
     ifs.close()
 
+    day = filepath[-12:-4]
     hasline = []
     i = 1
     pre = False
@@ -25,12 +21,28 @@ def main():
         elif pre and line[-2:-1] == '0':
             if len(hasline) <= 5:
                 continue
-            ofs = open(outpath + '/' + day + '-' + str(i) + '.txt', 'w')
+            if not os.path.exists(outpath):
+                os.mkdir(outpath)
+            if not os.path.exists(os.path.join(outpath, day)):
+                os.mkdir(os.path.join(outpath, day))
+            ofs = open(os.path.join(outpath, day, str(i) + '.txt'), 'w')
             ofs.writelines(hasline)
+            ofs.close()
             hasline = []
             i += 1
             pre = False
-            
+
+
+def main():
+    rootpath = '/dataPool/map/2015cabGPS_splitpath/'
+    outpath = '/dataPool/map/hasPeople/'
+    ids = os.listdir(rootpath)
+    for id in ids:
+        idpath = os.path.join(rootpath, id)
+        filenames = os.listdir(idpath)
+        for filename in filenames:
+            findHasLine(os.path.join(idpath, filename),
+                        os.path.join(outpath, id))
 
 if __name__ == '__main__':
     start = time.time()

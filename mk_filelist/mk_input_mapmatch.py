4332f713401e2fe18ab83799633f612a8da7fdfa
elvis2els <yulong_elvis@foxmail.com>
2017-03-06 20:36:43 +0800

    修改编写风格和逻辑

--- mk_filelist/mk_input_mapmatch.py
+++ mk_filelist/mk_input_mapmatch.py
@@ -1,33 +1,47 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
 import os
 import argparse
+import time
 
-parser = argparse.ArgumentParser(description="using to make mapmach input and ouput path file")
-parser.add_argument('path', help="input source path and ouput destination path", nargs=2)
+parser = argparse.ArgumentParser(
+    description="using to make mapmach input and ouput path file")
+parser.add_argument(
+    'path', help="input source path, ouput destination path and the inoutput file path", nargs=3)
+parser.add_argument(
+    'range', help="the index of cabids, from and to", nargs=2, type=int)
 args = parser.parse_args()
 
 
 def main():
     srcpath = args.path[0]
     despath = args.path[1]
+    inoutputfile = args.path[2]
     if not os.path.exists(srcpath):
         raise Exception("srcpath is not exists")
     inoutputPath = []
-    for srcroot, srcdirs, srcfiles in os.walk(srcpath):
-        for cabid in srcdirs:
-            cabidpath = srcpath + cabid
-            for cabidroot, cabiddir, cabidfiles in os.walk(cabidpath):
-                for cabidfile in cabidfiles:
-                    inoutputPath.append(cabidpath + '/' + cabidfile + '\n')
-                    inoutputPath.append("/map/cabGPS_mapmatch/"+ cabid + "/" + cabidfile
-                            + '\n')
-    f = open(despath, 'w')
+    cabids = os.listdir(srcpath)
+    pos = args.range[0]
+    maxpos = len(cabids)
+    while pos < args.range[1] and pos < maxpos:
+        id = cabids[pos]
+        cabidpath = os.path.join(srcpath, id)
+        days = os.listdir(cabidpath)
+        for day in days:
+            for cabidfile in os.listdir(os.path.join(cabidpath, day)):
+                inoutputPath.append(os.path.join(
+                    cabidpath, day, cabidfile) + '\n')
+                inoutputPath.append(os.path.join(
+                    despath , id, day, cabidfile) + '\n')
+        pos += 1
+    f = open(inoutputfile, 'w')
     f.writelines(inoutputPath)
     f.close()
 
 
 if __name__ == '__main__':
+    begin = time.time()
     if args.path:
-        try:
-            main()
-        except Exception as e:
-            print(e)
+        main()
+    end = time.time()
+    print(end - begin)

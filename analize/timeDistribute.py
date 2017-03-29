060d71b2bb189f0ce113a6a2f7e6a24eb209d086
elvis2els <yulong_elvis@foxmail.com>
2017-03-29 15:34:53 +0800

    去除异常后的分布

--- analize/timeDistribute.py
+++ analize/timeDistribute.py
@@ -1,83 +1,68 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 import argparse
-import datetime
+import datetime as dt
+import math
 import os
-import re
 
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 parser = argparse.ArgumentParser(description="show time distribute")
 parser.add_argument('file', help="the file of time duration")
-parser.add_argument('--week', choices=['weekday', 'weekend'],
-                    required=True, help="analize weekday or weekend")
-parser.add_argument(
-    '--mode', choices=['time', 'speed'], required=True, help="use time or speed")
-parser.add_argument('-c', '--clusFile', default=None,
-                    help='the path of vecluster')
+# parser.add_argument('--week', choices=['weekday', 'weekend'],
+#                     required=True, help="analize weekday or weekend")
+# parser.add_argument('-c', '--clusFile', default=None,
+#                     help='the path of vecluster')
 args = parser.parse_args()
 
 
-def main():
-    f = open(args.file, 'r')
-    lines = f.readlines()
-    for line in lines:
-        line = line.strip('\n').split(',')
-        datalimit = 0
-        date = datetime.datetime.strptime(line[1], '%Y-%m-%d %H:%M:%S.%f')
-        time = date.time()
-        time = datetime.timedelta(hours=time.hour, minutes=time.minute, seconds=time.second,
-                                  milliseconds=time.microsecond / 1000).total_seconds() / 3600
-        if args.mode == 'time':
-            timeduration = datetime.datetime.strptime(line[2], '%H:%M:%S.%f')
-            x, y = time, datetime.timedelta(hours=timeduration.hour, minutes=timeduration.minute,
-                                            seconds=timeduration.second, milliseconds=timeduration.microsecond / 1000).total_seconds()
-            datalimit = 1000
-        else:
-            x, y = time, float(line[3])
-            datalimit = 150
-        if y <= datalimit:
-            if args.week:
-                if args.week == 'weekday':
-                    if date.weekday() <= 4:
-                        plt.scatter(x, y)
-                elif args.week == 'weekend':
-                    if date.weekday() >= 5:
-                        plt.scatter(x, y)
-            else:
-                if date.weekday() <= 4:
-                    plt.scatter(x, y, c="red")
-                else:
-                    plt.scatter(x, y, c="blue")
-    f.close()
+def draw_weekdays_plot(df):
+    plt.plot(df['time_x'].dt.time, df['duration'], 'ob')
+    plt.show()
 
-    if args.clusFile:
-        f = open(os.path.expanduser(args.clusFile), 'r')
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip('\n').split('-')
-            time_split = [int(r) for r in re.split(':|\.', line[0])]
-            if len(time_split) != 4:
-                time_split.append(0)
-            clus_time_start = datetime.timedelta(hours=time_split[0], minutes=time_split[1],
-                                                 seconds=time_split[2], milliseconds=time_split[3] / 1000).total_seconds() / 3600
-            time_split = [int(r) for r in re.split(':|\.', line[1])]
-            if len(time_split) != 4:
-                time_split.append(0)
-            clus_time_end = datetime.timedelta(hours=time_split[0], minutes=time_split[1],
-                                                 seconds=time_split[2], milliseconds=time_split[3] / 1000).total_seconds() / 3600
-            plt.plot([clus_time_start, clus_time_end], [line[2], line[2]], c='red')
 
-    if args.mode == 'time':
-        plt.title('Time Distribute')
-        plt.xlabel('hour')
-        plt.ylabel('second')
+def is_outlier(row, state):
+    q1 = state.loc[row.time_group]['q1']
+    q3 = state.loc[row.time_group]['q3']
+    iqr = q3 - q1
+    if row.duration > (q3 + 1.5 * iqr) or row.duration < (q1 - 1.5 * iqr):
+        return True
     else:
-        plt.title('Speed Distribute')
-        plt.xlabel('hour')
-        plt.ylabel('km/h')
-    plt.legend()
-    plt.grid(True)
+        return False
+
+
+def main():
+    df = pd.read_csv(args.file)
+
+    # 列类型转换
+    df['time_x'], df['time_y'], df['duration'] = \
+        pd.to_datetime(df['time_x']), \
+        pd.to_datetime(df['time_y']), \
+        pd.to_timedelta(df['duration'])
+
+    df['duration'] = df['duration'] / np.timedelta64(1, 's')
+    df['time_group'] = ((df['time_x'].dt.hour * 60 +
+                         df['time_x'].dt.minute) / 5).apply(math.ceil)
+
+    print("原始长度： {}".format(len(df)))
+    grouped = df.groupby('time_group')
+    statBefore = pd.DataFrame({'q1': grouped['duration'].quantile(.25),
+                               'q3': grouped['duration'].quantile(.75)})
+    df['outlier'] = df.apply(is_outlier, axis=1, args=(statBefore,))
+    df = df[~(df.outlier)]
+    del df['outlier']
+    print("filtered长度： {}".format(len(df)))
+
+    df_weekday = df[df['time_x'].dt.weekday < 6]
+    df_weekend = df[df['time_x'].dt.weekday >= 6]
+    draw_weekdays_plot(df_weekday)
+    draw_weekdays_plot(df_weekend)
+
+    df_weekday.boxplot(column='duration', by='time_group')
+    plt.show()
+    df_weekend.boxplot(column='duration', by='time_group')
     plt.show()
 
 if __name__ == '__main__':

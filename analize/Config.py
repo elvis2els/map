45f9ea1ff7831c96f9372e21b6f50241aba46710
elvis <yulong_elvis@foxmail.com>
2017-05-11 11:15:20 +0800

    从配置文件中读参数

--- analize/Config.py
+++ analize/Config.py
@@ -1,6 +1,25 @@
+import configparser
+import os
+
+
 class Config(object):
     def __init__(self):
-        self.dbhost = 'localhost'
-        self.dbuser = 'root'
-        self.dbpasswd = '369212'
-        self.dbname = 'path_restore'
+        self.conf = 'default.conf'
+        if not os.path.exists(self.conf):
+            config = configparser.ConfigParser()
+            config['database'] = {'host': 'localhost',
+                                  'user': 'root',
+                                  'passwd': '369212',
+                                  'name': 'path_restore'}
+            config['analizeTime'] = {
+                'homepath': '/home/elvis/map/analize/analizeTime/countXEntTime/'}
+            with open(self.conf, 'w') as configfile:
+                config.write(configfile)
+
+    def getConf(self, section):
+        config = configparser.ConfigParser()
+        config.read('default.conf')
+        if section in config.sections():
+            return config[section]
+        else:
+            raise NameError('not have the section: [{}]'.format(section))

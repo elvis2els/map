e4ac8aea66b4bf608c90cf2567f846b3a81c2ea7
elvis2els <yulong_elvis@foxmail.com>
2017-09-27 13:35:11 +0800

    fix bug

--- path_restore/mainRoad_to_sql.py
+++ path_restore/mainRoad_to_sql.py
@@ -68,7 +68,7 @@ def main():
     values, i, maxlen = [], 1406, len(edges) + 1406
     query = 'INSERT INTO main_path (edge_meta_id, start_group_time, end_group_time, path, weekday) values(%s, %s, %s, "%s", %s);'
     time_s = time.time()
-    with futures.ProcessPoolExecutor(max_workers=3) as pool:
+    with futures.ProcessPoolExecutor() as pool:
         for meta_id, mfp_weekday, mfp_weekend in pool.map(getMainRoad, edges):
             values.extend(toValues(mfp_weekday, meta_id))
             values.extend(toValues(mfp_weekend, meta_id, weekday=False))

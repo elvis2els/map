43120296dfd91e5b2c07f81ca6758e679416b0e4
elvis2els <yulong_elvis@foxmail.com>
2017-10-18 14:38:35 +0800

    增加连接池

--- sql_about/add_path_to_metadata.py
+++ sql_about/add_path_to_metadata.py
@@ -1,24 +1,25 @@
 import pymysql
 import json
+from DBUtils.PooledDB import PooledDB
 
 if __name__ == '__main__':
     try:
-        connection = pymysql.connect(host='192.168.3.237',
-                                     user='root',
-                                     password='369212',
-                                     db='path_restore')
-        with connection.cursor() as cursor:
-            for metaid in range(1, 1218443):
-                query = "SELECT cross_id FROM traj_data WHERE metadata_id={}".format(metaid)
-                cursor.execute(query)
-                crosses = cursor.fetchall()
-                path = []
-                for cross in crosses:
-                    path.append(cross[0])
-                update = "UPDATE traj_metadata SET path='{path}' WHERE id={metaid}".format(path=json.dumps(path), metaid=metaid)
-                cursor.execute(update)
-                connection.commit()
-                if metaid % 1000 == 0:
-                    print(metaid)
+        pool = PooledDB(pymysql, 5, host='192.168.3.237', user='root', passwd='369212', db='path_restore', port=3306)
+        for metaid in range(172723, 1218443):
+            connection = pool.connection()
+            cursor = connection.cursor()
+            query = "SELECT cross_id FROM traj_data WHERE metadata_id={}".format(metaid)
+            cursor.execute(query)
+            crosses = cursor.fetchall()
+            path = []
+            for cross in crosses:
+                path.append(cross[0])
+            update = "UPDATE traj_metadata SET path='{path}' WHERE id={metaid}".format(path=json.dumps(path), metaid=metaid)
+            cursor.execute(update)
+            connection.commit()
+            cursor.close()
+            connection.close()
+            if metaid % 1000 == 0:
+                print(metaid)
     except Exception as e:
         print(str(e))

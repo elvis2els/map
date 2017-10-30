e4c4ce617a27a19358fba9a5b68fa43b874a5dcd
elvis2els <yulong_elvis@foxmail.com>
2017-10-30 16:00:52 +0800

    改为单进程

--- path_restore/important_cross_score.py
+++ path_restore/important_cross_score.py
@@ -6,8 +6,9 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 import numpy as np
 import pandas as pd
 
-from path_restore.gt_roadmap import RoadMap
+from gt_roadmap import RoadMap
 from sql_about.MysqlDB import MysqlDB
+from progressbar import Percentage, Bar, ETA, FileTransferSpeed, ProgressBar
 
 
 class ImportantCorss(object):
@@ -16,17 +17,32 @@ class ImportantCorss(object):
         self.map.load()
         self.db = MysqlDB(5, host='192.168.3.199', user='root', passwd='123456', db='path_restore')
 
+    # def compute_all_cross(self):
+    #     type_id = self.get_type_id('count*od_group')
+    #     with ProcessPoolExecutor(max_workers=1) as executor:
+    #         futures = {executor.submit(self.important_score, int(cross.__str__())): int(cross.__str__()) for cross in
+    #                    self.map.g.vertices()}
+    #         for future in as_completed(futures):
+    #             cross_id = futures[future]
+    #             try:
+    #                 score = future.result()
+    #                 self.score_to_sql(cross_id, score, type_id)
+    #             except Exception as e:
+    #                 print(str(e))
+
     def compute_all_cross(self):
         type_id = self.get_type_id('count*od_group')
-        with ProcessPoolExecutor(max_workers=16) as executor:
-            futures = {executor.submit(self.important_score(), cross) for cross in self.map.g.vertices()}
-            for future in as_completed(futures):
-                cross_id = futures[future]
-                try:
-                    score = future.result()
-                    self.score_to_sql(cross_id, score, type_id)
-                except Exception as e:
-                    print(str(e))
+        sum_num = self.map.g.num_vertices()
+        widgets = ['Insert: ', Percentage(), ' ', Bar(marker='#'), ' ', ETA()]
+        pbar = ProgressBar(widgets=widgets, maxval=sum_num).start()
+        for cross in self.map.g.vertices():
+            cross_id = int(cross.__str__())
+            try:
+                score = self.important_score(cross_id)
+                self.score_to_sql(cross_id, score, type_id)
+                pbar.update(cross_id)
+            except Exception as e:
+                print(str(e))
 
     def important_score(self, cross_id):
         """计算路口重要性分数"""
@@ -54,10 +70,10 @@ class ImportantCorss(object):
     def get_type_id(self, visual_type):
         """获取type对应id"""
         with self.db.cursor() as cursor:
-            query = "SELECT id FROM visual_type WHERE type = {}".format(visual_type)
+            query = "SELECT id FROM visual_type WHERE type_name = '{}'".format(visual_type)
             cursor.execute(query)
             type_id = cursor.fetchone()[0]
-        return type_id
+            return type_id
 
     def score_to_sql(self, cross_id, score, type_id):
         """计算好的结果存入数据库"""
--- sql_about/MysqlDB.py
+++ sql_about/MysqlDB.py
@@ -3,12 +3,14 @@
 
 import pymysql
 from DBUtils.PooledDB import PooledDB
+from contextlib import contextmanager
 
 
 class MysqlDB(object):
     def __init__(self, max_num, host, user, passwd, db, port=3306):
         self.pool = PooledDB(pymysql, max_num, host=host, user=user, passwd=passwd, db=db, port=port)
 
+    @contextmanager
     def cursor(self):
         try:
             connection = self.pool.connection()
